b9bf7699ac393d7e46bb920cb8ad584611e50bdf
AminTabite <Aminepicode13@gmail.com>
2025-09-16 15:21:49 +0200

    es 1 done

--- src/Entities/Es1.java
+++ src/Entities/Es1.java
@@ -1,26 +1,38 @@
 package Entities;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Es1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        for (int i =0; i < 6; i++) {
+        HashSet<String> parole = new HashSet<>();
+        ArrayList<String> paroleduplicate = new ArrayList<>();
+        System.out.println("Il numero di parole che vuoi inserire");
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
 
-            System.out.println("Quanti numeri vuoi inserire");
-            int n = scanner.nextInt();
-
-
-            System.out.println("Quante parole vuoi inserire?");
+            System.out.println("Quali parole vuoi inserire?");
 
             String nparole = scanner.nextLine();
 
-            Set<n>
-        }
 
+            if (parole.contains(nparole)) {
+
+
+                paroleduplicate.add(nparole);
+
+            } else {
+                parole.add(nparole);
+            }
+
+
+        }
+        System.out.println("set parole distinte :" + parole);
+        System.out.println("set parole duplicate :" + paroleduplicate);
+        System.out.println("parole distinte : " + parole.size());
 
 
     }
