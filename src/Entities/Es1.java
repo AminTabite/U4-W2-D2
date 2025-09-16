018b76bdcb35a71a9c8d95ef609494613fba6600
AminTabite <Aminepicode13@gmail.com>
2025-09-16 14:29:10 +0200

    start
    .

---
+++ src/Entities/Es1.java
@@ -0,0 +1,27 @@
+package Entities;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Es1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i =0; i < 6; i++) {
+
+            System.out.println("Quanti numeri vuoi inserire");
+            int n = scanner.nextInt();
+
+
+            System.out.println("Quante parole vuoi inserire?");
+
+            String nparole = scanner.nextLine();
+
+            Set<n>
+        }
+
+
+
+    }
+}
